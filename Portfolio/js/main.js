13f62fd650350104010df4da131427b0f6919f68
Mahmoud Hassan <60327828+mahmoudahassan@users.noreply.github.com>
2025-11-27 11:35:32 -0500

    Add smooth scroll functionality to nav menu

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -1,1 +1,10 @@
+// Smooth scroll for nav menu
+document.querySelectorAll("nav a").forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behaviour: "smooth"
+        });
+    });
+});
 
